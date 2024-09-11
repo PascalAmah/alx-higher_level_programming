4aa06ea3e92f3f9b732e7fadf9fdc6183e1d2fd2
PascalAmah <pascal.amah10@gmail.com>
2024-09-10 21:35:24 -0700

    debug task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(26):
-    print(chr(122 - i)
-            if i % 2 == 0
-            else chr(90 - i), end='')
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
