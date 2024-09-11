9b146b5ccf221adba558d10d1cbbdb566ef52e2d
PascalAmah <pascal.amah10@gmail.com>
2024-09-10 20:52:59 -0700

    Task 8 completed

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(s):
+    """Prints a string in uppercase followed by a new line."""
+
+    for c in s:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
