14a1dcf0f38bcdafd2bec665d7e88b8fdd825ed4
pascalamah <pascal.amah10@gmail.com>
2025-01-16 15:48:28 -0800

    debug

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets a response with "You got me!"
-curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me to receive "You got me!"
+curl -sL -X GET 0.0.0.0:5000/catch_me --data "user_id=98" -H "Origin: HolbertonSchool"
